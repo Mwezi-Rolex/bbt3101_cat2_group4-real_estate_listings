4fa723216302a9c8082c01ab40d77ed3369021c6
Mathenge Gitahi <mattgitahi@gmail.com>
2025-08-14 10:21:20 +0300

    writing the urls for the 5 models

---
+++ real_estate/listings/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from .views import AgentViewSet, PropertyViewSet, PropertyImageViewSet, ClientViewSet, BookingViewSet
+
+router = DefaultRouter()
+router.register(r'agents', AgentViewSet)
+router.register(r'properties', PropertyViewSet)
+router.register(r'property-images', PropertyImageViewSet)
+router.register(r'clients', ClientViewSet)
+router.register(r'bookings', BookingViewSet)
+
+urlpatterns = router.urls
