1ca04796e0aaa13cf6f1b0ece98c9d9e8e205be9
Mathenge Gitahi <mattgitahi@gmail.com>
2025-08-20 09:06:04 +0300

    improving admin interface

--- real_estate/listings/admin.py
+++ real_estate/listings/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
-from .models import Agent, Property, Client, Booking, PropertyImage  # import your models
+from .models import Agent, Property, Client, Booking, PropertyImage, PropertyType
 
-admin.site.register(Agent)
-admin.site.register(Property)
-admin.site.register(Client)
-admin.site.register(Booking)
-admin.site.register(PropertyImage)
+
+class PropertyImageInline(admin.TabularInline):
+    model = PropertyImage
+    extra = 1
+
+
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone", "agency", "license_number")
+    search_fields = ("name", "email", "agency", "license_number")
+
+
+@admin.register(PropertyType)
+class PropertyTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "location", "property_type", "agent", "date_listed")
+    search_fields = ("title", "location", "agent__name", "property_type__name")
+    list_filter = ("property_type", "agent", "date_listed")
+    inlines = [PropertyImageInline]
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone")
+    search_fields = ("name", "email")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("client", "property", "status", "date_sent")
+    search_fields = ("client__name", "property__title")
+    list_filter = ("status", "date_sent")
