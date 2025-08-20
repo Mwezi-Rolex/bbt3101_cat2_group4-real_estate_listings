63b88d4bf6f25c57cc8aef9f4de2a784f7cc141d
Mathenge Gitahi <mattgitahi@gmail.com>
2025-08-20 09:04:46 +0300

    fixing api routes

--- real_estate/real_estate/urls.py
+++ real_estate/real_estate/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('listings.urls')),
+   path('api/v1/', include('listings.urls')),
 ]
 
 if settings.DEBUG:
