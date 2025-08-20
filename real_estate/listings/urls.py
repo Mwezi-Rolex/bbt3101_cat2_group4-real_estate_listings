1a6c319542fa21309bc53bf0ad23c4046017c082
Mathenge Gitahi <mattgitahi@gmail.com>
2025-08-20 09:04:33 +0300

    adding the propertyType viewset

--- real_estate/listings/urls.py
+++ real_estate/listings/urls.py
@@ -1,8 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import AgentViewSet, PropertyViewSet, PropertyImageViewSet, ClientViewSet, BookingViewSet
+from .views import (
+    AgentViewSet, PropertyViewSet, PropertyImageViewSet,
+    ClientViewSet, BookingViewSet, PropertyTypeViewSet
+)
 
 router = DefaultRouter()
 router.register(r'agents', AgentViewSet)
+router.register(r'property-types', PropertyTypeViewSet)
 router.register(r'properties', PropertyViewSet)
 router.register(r'property-images', PropertyImageViewSet)
 router.register(r'clients', ClientViewSet)
